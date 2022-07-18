857a4099d401ddaaf65948b7308bf8dd8c13f4a0
AdrianArnaiz <adrian@ellisalicante.org>
2022-07-18 15:21:05 +0200

    Changes in SDRF Transformation for GRAPH CLF

--- transforms/sdrf/sdrf_transform.py
+++ transforms/sdrf/sdrf_transform.py
@@ -1,5 +1,50 @@
+import torch
 from torch_geometric.transforms import BaseTransform
-
+from torch_geometric.data import Data
+from transforms.sdrf.curvature import sdrf
+from transforms.sdrf.utils import get_dataset
 
 class SDRF(BaseTransform):
-    pass
+    
+    def __init__(self,
+        max_steps: int = None,
+        remove_edges: bool = True,
+        removal_bound: float = 0.5,
+        tau: float = 1,
+        undirected: bool = False,
+        use_edge_weigths = True
+        ):
+        
+        self.max_steps = int(max_steps)
+        self.remove_edges = remove_edges
+        self.removal_bound = removal_bound
+        self.tau = tau
+        self.undirected = undirected
+        self.use_edge_weigths = use_edge_weigths
+
+    def __call__(self, graph_data):
+
+        graph_data = get_dataset(graph_data, use_lcc=True)
+
+        altered_data = sdrf(
+            graph_data,
+            loops=self.max_steps,
+            remove_edges=self.remove_edges,
+            removal_bound=self.removal_bound,
+            tau=self.tau,
+            is_undirected=self.undirected,
+        )
+
+        new_data = Data(
+            edge_index=torch.LongTensor(altered_data.edge_index),
+            edge_attr=torch.FloatTensor(altered_data.edge_attr) if altered_data.edge_attr is not None else None,
+            y=graph_data.y,
+            x=graph_data.x,
+            num_nodes = graph_data.num_nodes
+        )      
+        return new_data
+
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}({self.max_steps})'
+
--- transforms/sdrf/utils.py
+++ transforms/sdrf/utils.py
@@ -1,41 +1,71 @@
 import numpy as np
-from torch_geometric.data import InMemoryDataset
-
+from torch_geometric.data import Data
+import torch
 
 def softmax(a, tau=1):
     exp_a = np.exp(a * tau)
     return exp_a / exp_a.sum()
 
 
-def get_adj_matrix(dataset: InMemoryDataset) -> np.ndarray:
-    num_nodes = dataset.data.x.shape[0]
-    adj_matrix = np.zeros(shape=(num_nodes, num_nodes))
-    for i, j in zip(dataset.data.edge_index[0], dataset.data.edge_index[1]):
-        adj_matrix[i, j] = 1.0
-    return adj_matrix
+def get_node_mapper(lcc: np.ndarray) -> dict:
+    mapper = {}
+    counter = 0
+    for node in lcc:
+        mapper[node] = counter
+        counter += 1
+    return mapper
 
 
-def get_undirected_adj_matrix(dataset: InMemoryDataset) -> np.ndarray:
-    num_nodes = dataset.data.x.shape[0]
-    adj_matrix = np.zeros(shape=(num_nodes, num_nodes))
-    for i, j in zip(dataset.data.edge_index[0], dataset.data.edge_index[1]):
-        adj_matrix[i, j] = 1.0
-        adj_matrix[j, i] = 1.0
-    return adj_matrix
+def remap_edges(edges: list, mapper: dict) -> list:
+    row = [e[0] for e in edges]
+    col = [e[1] for e in edges]
+    row = list(map(lambda x: mapper[x], row))
+    col = list(map(lambda x: mapper[x], col))
+    return [row, col]
 
 
-def get_top_k_matrix(A: np.ndarray, k: int = 128) -> np.ndarray:
-    num_nodes = A.shape[0]
-    row_idx = np.arange(num_nodes)
-    A[A.argsort(axis=0)[: num_nodes - k], row_idx] = 0.0
-    norm = A.sum(axis=0)
-    norm[norm <= 0] = 1  # avoid dividing by zero
-    return A / norm
+def get_component(dataset, start: int = 0) -> set:
+    visited_nodes = set()
+    queued_nodes = set([start])
+    row, col = dataset.edge_index.numpy()
+    while queued_nodes:
+        current_node = queued_nodes.pop()
+        visited_nodes.update([current_node])
+        neighbors = col[np.where(row == current_node)[0]]
+        neighbors = [
+            n for n in neighbors if n not in visited_nodes and n not in queued_nodes
+        ]
+        queued_nodes.update(neighbors)
+    return visited_nodes
 
 
-def get_clipped_matrix(A: np.ndarray, eps: float = 0.01) -> np.ndarray:
-    num_nodes = A.shape[0]
-    A[A < eps] = 0.0
-    norm = A.sum(axis=0)
-    norm[norm <= 0] = 1  # avoid dividing by zero
-    return A / norm
+def get_largest_connected_component(dataset) -> np.ndarray:
+    remaining_nodes = set(range(dataset.x.shape[0]))
+    comps = []
+    while remaining_nodes:
+        start = min(remaining_nodes)
+        comp = get_component(dataset, start)
+        comps.append(comp)
+        remaining_nodes = remaining_nodes.difference(comp)
+    return np.array(list(comps[np.argmax(list(map(len, comps)))]))
+
+
+def get_dataset(dataset: Data, use_lcc: bool = True):
+    if use_lcc:
+        lcc = get_largest_connected_component(dataset)
+
+        x_new = dataset.x[lcc]
+        y_new = dataset.y # for graph clf, same y
+
+        row, col = dataset.edge_index.numpy()
+        edges = [[i, j] for i, j in zip(row, col) if i in lcc and j in lcc]
+        edges = remap_edges(edges, get_node_mapper(lcc))
+
+        data = Data(
+            x=x_new,
+            edge_index=torch.LongTensor(edges),
+            y=y_new
+        )
+        dataset = data
+
+    return dataset
