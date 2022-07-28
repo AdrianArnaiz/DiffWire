12fc4464378161a74082d09143d97d230e34a74b
Adrián Arnaiz <adrian@ellisalicante.org>
2022-07-28 14:13:24 +0200

    Add Knn preprocessing

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -1,3 +1,4 @@
 from transforms.sdrf.sdrf_transform import SDRF
 from transforms.transform_features import FeatureDegree
 from transforms.transform_features import DIGLedges
+from transforms.transform_features import KNNGraph
--- transforms/transform_features.py
+++ transforms/transform_features.py
@@ -1,8 +1,9 @@
 import torch
 import torch.nn.functional as F
 
+import torch_geometric
 from torch_geometric.transforms import BaseTransform
-from torch_geometric.utils import degree
+from torch_geometric.utils import degree, to_undirected
 from torch_geometric.utils.convert import to_scipy_sparse_matrix, from_scipy_sparse_matrix
 
 import scipy.sparse as sp
@@ -109,3 +110,75 @@ class DIGLedges(BaseTransform):
     
     def __repr__(self) -> str:
         return f'{self.__class__.__name__}(alpha={self.alpha}, eps={self.alpha})'
+
+
+class KNNGraph(BaseTransform):
+    r"""Creates a k-NN graph based on node positions :obj:`pos`
+    (functional name: :obj:`knn_graph`).
+
+    Args:
+        k (int, optional): The number of neighbors. (default: :obj:`6`)
+        loop (bool, optional): If :obj:`True`, the graph will contain
+            self-loops. (default: :obj:`False`)
+        force_undirected (bool, optional): If set to :obj:`True`, new edges
+            will be undirected. (default: :obj:`False`)
+        flow (string, optional): The flow direction when used in combination
+            with message passing (:obj:`"source_to_target"` or
+            :obj:`"target_to_source"`).
+            If set to :obj:`"source_to_target"`, every target node will have
+            exactly :math:`k` source nodes pointing to it.
+            (default: :obj:`"source_to_target"`)
+        cosine (boolean, optional): If :obj:`True`, will use the cosine
+            distance instead of euclidean distance to find nearest neighbors.
+            (default: :obj:`False`)
+        num_workers (int): Number of workers to use for computation. Has no
+            effect in case :obj:`batch` is not :obj:`None`, or the input lies
+            on the GPU. (default: :obj:`1`)
+    """
+    def __init__(
+        self,
+        k=None,
+        loop=False,
+        force_undirected=True,
+        flow='source_to_target',
+        cosine: bool = False,
+        num_workers: int = 1,
+    ):
+        self.k = k
+        self.loop = loop
+        self.force_undirected = force_undirected
+        self.flow = flow
+        self.cosine = cosine
+        self.num_workers = num_workers
+
+    def __call__(self, data):
+        data.edge_attr = None
+        batch = data.batch if 'batch' in data else None
+
+        if data.x is None:
+            idx = data.edge_index[1 if self.in_degree else 0]
+            deg = degree(idx, data.num_nodes, dtype=torch.float).unsqueeze(-1)
+            data.x = deg
+
+        if self.k is None:
+            self.k = int(2*data.num_edges / data.num_edges)
+
+        edge_index = torch_geometric.nn.knn_graph(
+            data.x,
+            self.k,
+            batch,
+            loop=self.loop,
+            flow=self.flow,
+            cosine=self.cosine,
+            num_workers=self.num_workers,
+        )
+
+        if self.force_undirected:
+            edge_index = to_undirected(edge_index, num_nodes=data.num_nodes)
+
+        data.edge_index = edge_index
+
+        return data
+
+    def __repr__(self) -> str:
+        return f'{self.__class__.__name__}(k={self.k})'
